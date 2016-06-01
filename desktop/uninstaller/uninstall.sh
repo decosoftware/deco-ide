423ed2aa8399bf8bade71a0a9d2e5dd1d9f4e79d
GJ Nilsen <gert@yuppielabel.com>
2016-06-01 22:39:34 +0200

    Create uninstall.sh
    Uninstaller script

---
+++ desktop/uninstaller/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm /usr/local/Deco
+sudo rm ~/Library/Application Support/com.decosoftware.Deco
+sudo rm /usr/local/var/run/watchman
+sudo rm ~/.Deco
+sudo rm /Applications/Deco.app
