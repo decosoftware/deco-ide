9f6b2b70ca8c8e5f948c2e61463dfd486e884419
Gavin Owens <owens.gdo@gmail.com>
2016-06-27 12:10:06 -0700

    bump build number to 0.7.0

--- desktop/gulpfile.js
+++ desktop/gulpfile.js
@@ -11,7 +11,7 @@ var plist = require('plist');
 var $ = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 
-var BUILD_VERSION = "0.6.0";
+var BUILD_VERSION = "0.7.0";
 
 var SIGN_PACKAGE = process.env['SIGN_DECO_PACKAGE'] == 'true'
 
