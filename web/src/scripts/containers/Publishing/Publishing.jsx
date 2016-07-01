f5dbb2fdf993903e3f1cfefca2627b3855f291d1
Devin Abbott <devinaabbott@gmail.com>
2016-07-01 16:15:44 -0700

    Publishing editor (#123)
    * use react-toggle-button (#104) (#111)
    
    👍
    
    * React update (#112)
    
    * use react-toggle-button (#104)
    
    👍
    
    * Update to react 15.1.0
    
    * Update Electron and add React Dev Tools (#113)
    
    * fix devTools variable name typo
    
    * Create publishing sidebar with login and landing pages (#114)
    
    * Rename publishing containers (#117)
    
    * Add metadata editor

--- web/src/scripts/containers/Publishing/Publishing.jsx
+++ web/src/scripts/containers/Publishing/Publishing.jsx
@@ -16,10 +16,12 @@
  */
 
 import React, { Component, } from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 
 import PublishingSignIn from './PublishingSignIn'
 import PublishingBrowser from './PublishingBrowser'
+import PublishingMetadata from './PublishingMetadata'
 import {PaneHeader} from '../../components'
 
 const styles = {
@@ -35,7 +37,10 @@ const styles = {
   }
 }
 
-const PublishingInspector = ({signedIn, user, components}) => {
+const PublishingInspector = ({signedIn, user, components, currentComponentId, width}) => {
+  const currentComponent = currentComponentId ?
+    _.find(components, ['id', currentComponentId]) : null
+
   return (
     <div style={styles.container}>
       <PaneHeader
@@ -44,10 +49,18 @@ const PublishingInspector = ({signedIn, user, components}) => {
         onClickRightTitle={() => {console.log('Sign out?')}}
       />
       {signedIn ? (
-        <PublishingBrowser
-          user={user}
-          components={components}
-        />
+        currentComponent ? (
+          <PublishingMetadata
+            user={user}
+            component={currentComponent}
+            width={width}
+          />
+        ) : (
+          <PublishingBrowser
+            user={user}
+            components={components}
+          />
+        )
       ) : (
         <PublishingSignIn />
       )}
@@ -64,19 +77,47 @@ const mapStateToProps = (state) => {
       lastname: 'Abbott',
       thumbnail: 'https://avatars0.githubusercontent.com/u/1198882?v=3&s=460',
     },
+    currentComponentId: '1',
     components: [
       {
         name: 'Button',
+        id: '1',
         repository: 'dabbott/button',
         downloads: 214,
+        props: [
+          {
+            value: 'Press me!',
+            metadata: {
+              name: 'title',
+              type: 'string',
+              editWith: 'inputField',
+            },
+          },
+          {
+            value: 'rgb(210,210,210)',
+            metadata: {
+              name: 'textColor',
+              type: 'string',
+              editWith: 'colorPicker',
+            },
+          },
+        ],
+        dependencies: {
+          'react-native': '*',
+        },
+        imports: {
+          'react-native-button': 'Button',
+        },
       },
       {
         name: 'PhotoGrid',
+        id: '2',
         repository: 'dabbott/photo-grid',
         downloads: 88,
       },
       {
         name: 'Lightbox',
+        id: '3',
         repository: 'dabbott/lightbox',
         downloads: 43,
       },
