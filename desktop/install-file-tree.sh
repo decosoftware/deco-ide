555d6ed4be15129174c34a3291c0a0a5f598bf7b
Gavin Owens <owens.gdo@gmail.com>
2016-08-19 12:11:51 -0700

    add file tree requires

--- desktop/install-file-tree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-common
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-server
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-server-transport-electron
-    
-
--- web/install-file-tree.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-common
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-client
-npm install ~/UIX-Workspace/file-tree/packages/file-tree-client-transport-electron
-npm install ~/UIX-Workspace/file-tree/packages/react-file-tree
-	
-
