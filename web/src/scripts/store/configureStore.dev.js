043d39b140f31f32b697bf43bdc70406c80f9f95
Devin Abbott <devinabbott@gmail.com>
2016-09-09 15:53:50 -0700

    Batch react updates to prevent double re-render on dispatch

--- web/src/scripts/store/configureStore.dev.js
+++ web/src/scripts/store/configureStore.dev.js
@@ -19,6 +19,9 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { syncHistory } from 'react-router-redux'
 import { hashHistory } from 'react-router'
 import thunk from 'redux-thunk'
+import { batchedSubscribe } from 'redux-batched-subscribe'
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
+
 import rootReducer from '../reducers'
 import loggingMiddleware from './loggingMiddleware'
 import ipcActionEmitter from '../ipc/ipcActionEmitter'
@@ -34,7 +37,8 @@ import DevTools from '../containers/DevTools'
 const reduxRouterMiddleware = syncHistory(hashHistory)
 const enhancer = compose(
   applyMiddleware(thunk, reduxRouterMiddleware, loggingMiddleware),
-  DevTools.instrument()
+  DevTools.instrument(),
+  batchedSubscribe(batchedUpdates)
 )
 
 function getDebugSessionKey() {
