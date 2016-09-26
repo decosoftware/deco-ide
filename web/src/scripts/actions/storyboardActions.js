b172f0a22dc05278fe01bbbd9eef1e792d50c5c1
Gabe G'Sell <gabergg@gmail.com>
2016-09-26 14:24:14 -0700

    pull out NewSceneButton, add comments

--- web/src/scripts/actions/storyboardActions.js
+++ web/src/scripts/actions/storyboardActions.js
@@ -14,6 +14,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 import _ from 'lodash'
 import path from 'path'
 
--- web/src/scripts/containers/Storyboard.jsx
+++ web/src/scripts/containers/Storyboard.jsx
@@ -1,3 +1,20 @@
+/**
+ *    Copyright (C) 2015 Deco Software Inat.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import React, { Component, PropTypes, } from 'react'
 import _ from 'lodash'
 import { connect } from 'react-redux'
@@ -9,6 +26,7 @@ import YOPS from 'yops'
 import * as ContentLoader from '../api/ContentLoader'
 import * as URIUtils from '../utils/URIUtils'
 import { storyboardActions } from '../actions'
+import NewSceneButton from '../components/storyboard/NewSceneButton'
 
 const stylesCreator = (theme) => {
   const {colors} = theme
@@ -32,38 +50,6 @@ const mapStateToProps = (state) => createSelector(
     scenes: storyboard.scenes,
   })
 )
-
-let newSceneCounter = 1
-
-class NewSceneButton extends Component {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-  }
-
-  render() {
-    const style = {
-      position: 'absolute',
-      background: 'black',
-      width: '30px',
-      height: '30px',
-      borderRadius: '25%',
-      top: '20px',
-      right: '20px',
-      zIndex: 1,
-      textAlign: 'center',
-      paddingTop: '3px',
-      color: 'white',
-      fontWeight: 'bold',
-      cursor: 'pointer',
-    }
-
-    return (
-      <div style={style} onClick={this.props.onClick}>
-        +
-      </div>
-    )
-  }
-}
 
 @StylesEnhancer(stylesCreator)
 class Storyboard extends Component {
