c88f2437e43d0466e218a9fb995432d16cb459b6
Devin Abbott <devinabbott@gmail.com>
2016-10-06 15:12:37 -0700

    Patch AppRegistry to support dynamic app entry

--- desktop/libs/Project/decoentry.js
+++ desktop/libs/Project/decoentry.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
 
-// modify this variable to move around components and screens
-let Entry = require('./index')
+// Patch `AppRegistry.registerComponent` on first launch
+if (!global.originalRegister) {
+  global.hasRegistered = false
+  global.hasRegisteredOnce = false
+  global.getComponent = null
 
-// point to index if this is a release build
-if (!__DEV__) {
-  Entry = require('./index')
-}
+  global.originalRegister = AppRegistry.registerComponent
 
-// Check if we're dealing with ES6-style imports
-if (typeof Entry === 'object' && Entry !== null && Entry.default) {
-
-  // If we are, pick the default import
-  Entry = Entry.default
-}
-
-class Root extends Component {
-  render() {
-    return <Entry/>
+  AppRegistry.registerComponent = (appKey, fn) => {
+    global.hasRegistered = true
+    global.hasRegisteredOnce = true
+    global.getComponent = fn
   }
 }
 
-AppRegistry.registerComponent('Project', () => Root)
+// On hot reload, we want to reset registration. There's a caveat, however:
+// Because registration order isn't gauranteed, we may still want to display
+// the previously registered component (which will patch itself, and not be stale)
+// so don't reset `global.getComponent`
+module.hot.accept(() => {
+  global.hasRegistered = false
+})
+
+class ProxyRoot extends Component {
+  render() {
+    let Entry = require("./index.ios")
+
+    // Require interop
+    if (typeof Entry === 'object' && Entry !== null && Entry.__esModule) {
+      Entry = Entry.default
+    }
+
+    // Try to use the registered component, falling back to the `require`d component
+    let EntryComponent = global.hasRegistered ? global.getComponent() : Entry
+
+    // Due to race conditions in component patching (I think), switching to a
+    // registered component may have happened and then been reset. However, the
+    // `global.getComponent` call will still return the correct component.
+    // Assume this is the case if we get an invalid component from `require`.
+    if (typeof EntryComponent === 'object') {
+      if (global.hasRegisteredOnce) {
+        EntryComponent = global.getComponent()
+
+      // If all else fails, return null and render nothing
+      } else {
+        console.warn(
+          'Failed to find component registered with AppRegistry.registerComponent or ' +
+          'exported by the app entry file.'
+        )
+        return null
+      }
+    }
+
+    return <EntryComponent />
+  }
+}
+
+global.originalRegister('Project', () => ProxyRoot)
